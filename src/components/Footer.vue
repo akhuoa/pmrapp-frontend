54d59efa2c38b09539d77be8bec4650bc7b47b77
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-12 18:55:12 +1300

    Update footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer>
-    <p>Copyright 2025 IUPS Physiome Project</p>
+  <footer class="bg-[#3a3a3a] text-[#868686] py-4">
+    <div class="container mx-auto px-4">
+      <p class="text-sm font-light">Copyright 2025 IUPS Physiome Project</p>
+    </div>
   </footer>
 </template>
