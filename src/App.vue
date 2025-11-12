1c5fcce796c4d6484a720f763a1a1605f9e6e809
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-12 16:06:50 +1300

    Add logo and favicon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="app-container">
+    <img src="/logo.png" alt="PMR Logo" class="logo" />
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-container {
+  text-align: center;
+}
+
+.logo {
+  max-width: 200px;
+  height: auto;
+  margin: 2rem 0;
+}
+</style>
