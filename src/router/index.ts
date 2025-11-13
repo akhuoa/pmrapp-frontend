ce449c88cbcef20b5fc29690de8cf87537fa500f
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-13 19:06:57 +1300

    Update page titles for different page views

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import ExposureView from '@/views/ExposureView.vue'
 import LoginView from '@/views/LoginView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 
+const title = 'Physiome Model Repository'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,33 +14,46 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { title: title },
     },
     {
       path: '/workspace',
       name: 'workspace',
       component: WorkspaceView,
+      meta: { title: `Workspace – ${title}` },
     },
     {
       path: '/workspace/:alias',
       name: 'workspace-detail',
       component: WorkspaceDetailView,
+      meta: { title: `Workspace – ${title}` },
     },
     {
       path: '/exposure',
       name: 'exposure',
       component: ExposureView,
+      meta: { title: `Exposure – ${title}` },
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: { title: `Login – ${title}` },
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: NotFoundView,
+      meta: { title: `Page Not Found – ${title}` },
     },
   ],
+})
+
+router.afterEach((to) => {
+  const title = to.meta.title as string | undefined
+  if (title) {
+    document.title = title
+  }
 })
 
 export default router
