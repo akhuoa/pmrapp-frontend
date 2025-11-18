076bf38e0231da7f8943bd51e508d3c1130cf677
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-18 13:49:38 +1300

    Add back to top button

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import BackToTop from '@/components/BackToTop.vue'
+</script>
+
 <template>
   <RouterView />
+  <BackToTop />
 </template>
---
+++ src/components/BackToTop.vue
@@ -0,0 +1,62 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isVisible = ref(false)
+
+const checkScroll = () => {
+  // Show button if user has scrolled down more than 300px
+  isVisible.value = window.scrollY > 300
+}
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', checkScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', checkScroll)
+})
+</script>
+
+<template>
+  <Transition name="fade">
+    <button
+      v-if="isVisible"
+      @click="scrollToTop"
+      class="fixed bottom-8 right-8 bg-[#830a28] hover:bg-[#d11241] text-white p-3 rounded-full shadow-lg hover:shadow-xl transition-all z-40"
+      aria-label="Back to top"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M11.47 2.47a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06l-6.22-6.22V21a.75.75 0 01-1.5 0V4.81l-6.22 6.22a.75.75 0 11-1.06-1.06l7.5-7.5z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+  </Transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
