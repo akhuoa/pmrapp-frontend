da2c3c8c0edd13475ac2467df3ba90fd376af6ab
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-20 18:39:10 +1300

    Update footer dark mode color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="bg-dark text-dark-light py-4">
+  <footer class="bg-dark dark:bg-gray-900 text-dark-light dark:text-gray-400 py-4">
     <div class="container mx-auto px-4">
       <p class="text-sm font-light">Copyright 2025 IUPS Physiome Project</p>
     </div>
