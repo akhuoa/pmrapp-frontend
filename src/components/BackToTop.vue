3fb4edabb767110a58f83f676de9ffabfd6f13e0
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-20 15:31:34 +1300

    Refactor svg icons

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,6 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
+import ArrowUpIcon from './icons/ArrowUpIcon.vue'
 
 const isVisible = ref(false)
 
@@ -33,18 +34,7 @@ onUnmounted(() => {
       class="fixed bottom-8 right-8 button-rounded-full z-40"
       aria-label="Back to top"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M11.47 2.47a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06l-6.22-6.22V21a.75.75 0 01-1.5 0V4.81l-6.22 6.22a.75.75 0 11-1.06-1.06l7.5-7.5z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <ArrowUpIcon />
     </button>
   </Transition>
 </template>
