4577921d5abeac7641c1f21621aa2e253e466022
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-20 19:35:21 +1300

    Update footer theme color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="bg-dark dark:bg-gray-900 text-dark-light dark:text-gray-400 py-4">
-    <div class="container mx-auto px-4">
+  <footer class="bg-dark dark:bg-gray-800 text-dark-light dark:text-gray-400 py-4">
+    <div class="container mx-auto px-4 text-center">
       <p class="text-sm font-light">Copyright 2025 IUPS Physiome Project</p>
     </div>
   </footer>
