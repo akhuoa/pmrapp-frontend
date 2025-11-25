ac7332c471d75df7887f81864c9cdb9e66e3d9df
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-11-25 13:29:06 +1300

    Add cookie storage for notification dismissed

--- src/components/molecules/MockDataNotification.vue
+++ src/components/molecules/MockDataNotification.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { USE_MOCK_DATA } from '@/services'
 import CloseButton from '@/components/atoms/CloseButton.vue'
 
+const COOKIE_NAME = 'pmr_mock_notification_dismissed'
+const COOKIE_DAYS = 7
+
 const isVisible = ref(true)
+
+const getCookie = (name: string): string | null => {
+  const nameEQ = name + '='
+  const cookies = document.cookie.split(';')
+  for (let cookie of cookies) {
+    cookie = cookie.trim()
+    if (cookie.indexOf(nameEQ) === 0) {
+      return cookie.substring(nameEQ.length)
+    }
+  }
+  return null
+}
+
+const setCookie = (name: string, value: string, days: number) => {
+  const date = new Date()
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+  const expires = 'expires=' + date.toUTCString()
+  document.cookie = `${name}=${value};${expires};path=/`
+}
+
+const handleClose = () => {
+  isVisible.value = false
+  setCookie(COOKIE_NAME, 'true', COOKIE_DAYS)
+}
+
+onMounted(() => {
+  if (getCookie(COOKIE_NAME) === 'true') {
+    isVisible.value = false
+  }
+})
 </script>
 
 <template>
@@ -19,7 +52,7 @@ const isVisible = ref(true)
     </div>
     <CloseButton
       class="absolute top-1/2 right-4 -translate-y-1/2"
-      @click="isVisible = false"
+      @click="handleClose"
       aria-label="Close notification"
     />
   </div>
