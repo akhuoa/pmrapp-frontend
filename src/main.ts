285d2270eee1c3b155ade10dd29482a29befc113
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-12-01 17:28:01 +1300

    Configure gtag

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createGtag } from 'vue-gtag'
 import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
 
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID
+const gtag = createGtag({
+  tagId: GA_MEASUREMENT_ID,
+  pageTracker: {
+    router,
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(gtag)
 
 app.mount('#app')
