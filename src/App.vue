a08f4b46586955db983f1cfbbcf93a8af18fc30b
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-12-08 19:21:06 +1300

    Remove notification-bar component usage form the app.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import BackToTop from '@/components/BackToTop.vue'
-import NotificationBar from '@/components/molecules/NotificationBar.vue'
-
-const showNotification = import.meta.env.VITE_SHOW_NOTIFICATION === 'true'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <NotificationBar v-if="showNotification" />
     <RouterView />
     <BackToTop />
   </div>
