2f81c9494fac87a51df311a128a91b75ac24accb
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-12-09 11:36:07 +1300

    Add user login and logout features

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+
+// Initialise authentication state from local storage.
+const authStore = useAuthStore()
+authStore.initAuth()
 
 app.mount('#app')
