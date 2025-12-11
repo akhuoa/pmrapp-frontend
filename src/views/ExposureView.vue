a66054d2bd6f9e2f23cf1c15cfe457e59ac38732
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-12-12 12:36:56 +1300

    Update layout so that header and footer will not refresh on route change

--- src/views/ExposureView.vue
+++ src/views/ExposureView.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import Exposures from '@/components/Exposures.vue'
 import PageHeader from '@/components/molecules/PageHeader.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
 </script>
 
 <template>
-  <DefaultLayout>
-    <PageHeader
-      title="Exposure Listing"
-      description="View and manage model exposures."
-    />
+  <PageHeader
+    title="Exposure Listing"
+    description="View and manage model exposures."
+  />
 
-    <Exposures />
-  </DefaultLayout>
+  <Exposures />
 </template>
--- src/views/WorkspaceDetailView.vue
+++ src/views/WorkspaceDetailView.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import WorkspaceDetail from '@/components/WorkspaceDetail.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const route = useRoute()
 const alias = route.params.alias as string
 </script>
 
 <template>
-  <DefaultLayout>
-    <WorkspaceDetail :alias="alias" />
-  </DefaultLayout>
+  <WorkspaceDetail :alias="alias" />
 </template>
