9ca1c104f386e10da39fefdd60c81450e650da07
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-12-16 15:18:13 +1300

    Make all icons sizes same

--- src/components/molecules/UserDropdown.vue
+++ src/components/molecules/UserDropdown.vue
@@ -53,7 +53,7 @@ onUnmounted(() => {
       class="nav-link p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
       aria-label="User menu"
     >
-      <UserIcon :size="20" />
+      <UserIcon />
     </button>
 
     <div
