803d74f1d5dd4bff078b03b58ec90d02268fbfae
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-01-06 18:25:11 +1300

    Update button label for Open in OpenCOR

--- src/components/ExposureDetail.vue
+++ src/components/ExposureDetail.vue
@@ -230,7 +230,7 @@ onMounted(async () => {
                 @click="handleClick"
               >
                 <span class="text-foreground">›</span>
-                Open in OpenCOR's Web App
+                Open in OpenCOR's Web app
               </a>
             </li>
           </ul>
