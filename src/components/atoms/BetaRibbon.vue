a36a95cabd01c2c3eeed27805b95a4e27bdad18c
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-01-30 14:25:31 +1300

    Add beta ribbon

---
+++ src/components/atoms/BetaRibbon.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="fixed w-[156px] bottom-0 left-[-50px] bg-primary text-white py-2 text-center rotate-45 z-999"
+    aria-label="This site is in beta development"
+    title="This site is in beta development"
+  >
+    <span class="border border-dashed border-gray-200 p-2 inline-block w-full text-sm font-mono">
+      BETA
+    </span>
+  </div>
+</template>
