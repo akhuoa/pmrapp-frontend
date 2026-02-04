8017bfca69fccb8ffb0c483dcb585fc7a0385b55
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-02-04 15:39:00 +1300

    Update accessibility

--- src/components/organisms/Search.vue
+++ src/components/organisms/Search.vue
@@ -161,7 +161,12 @@ const pushSearchQuery = (searchKind: string, searchTerm: string) => {
           @focus="isSearchFocused = true"
           @blur="isSearchFocused = false"
         />
-        <button class="px-4 py-2" @click="handleSearch">
+        <button
+          type="button"
+          class="px-4 py-2"
+          aria-label="Search"
+          @click="handleSearch"
+        >
           <SearchIcon class="w-5 h-5" />
           <span class="sr-only">Search</span>
         </button>
