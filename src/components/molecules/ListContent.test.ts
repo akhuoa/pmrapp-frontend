9e93c20a0ab736b70726d0b8ef3ca350ec4af338
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-02-17 15:11:49 +1300

    Update unit test for listContent component

--- src/components/molecules/ListContent.test.ts
+++ src/components/molecules/ListContent.test.ts
@@ -1,10 +1,10 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
-import ListContainer from '@/components/molecules/ListContainer.vue'
+import ListContent from '@/components/molecules/ListContent.vue'
 
-describe('ListContainer', () => {
+describe('ListContent', () => {
   it('renders error when error prop is provided', () => {
-    const wrapper = mount(ListContainer, {
+    const wrapper = mount(ListContent, {
       props: {
         items: [],
         error: 'Something went wrong',
@@ -26,7 +26,7 @@ describe('ListContainer', () => {
   })
 
   it('shows loading message when isLoading is true', () => {
-    const wrapper = mount(ListContainer, {
+    const wrapper = mount(ListContent, {
       props: {
         items: [],
         error: null,
@@ -45,7 +45,7 @@ describe('ListContainer', () => {
   })
 
   it('shows empty message when items array is empty', () => {
-    const wrapper = mount(ListContainer, {
+    const wrapper = mount(ListContent, {
       props: {
         items: [],
         error: null,
@@ -64,7 +64,7 @@ describe('ListContainer', () => {
   })
 
   it('renders slot content when items exist', () => {
-    const wrapper = mount(ListContainer, {
+    const wrapper = mount(ListContent, {
       props: {
         items: [{ id: 1 }, { id: 2 }],
         error: null,
@@ -85,7 +85,7 @@ describe('ListContainer', () => {
   })
 
   it('has box class when rendering items', () => {
-    const wrapper = mount(ListContainer, {
+    const wrapper = mount(ListContent, {
       props: {
         items: [{ id: 1 }],
         error: null,
