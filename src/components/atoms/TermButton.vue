57ab81596abb09fd546e9ac4a1d1d1e09ceabc9a
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-02-20 13:29:46 +1300

    Update term button focus style

--- src/components/atoms/TermButton.vue
+++ src/components/atoms/TermButton.vue
@@ -4,7 +4,24 @@ interface Props {
   active?: boolean
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const buttonBaseClass = [
+  'px-3',
+  'py-1.5',
+  'text-gray-600',
+  'dark:text-gray-300',
+  'rounded-md',
+  'text-sm',
+  'transition-colors',
+  'relative',
+  'focus:outline-none',
+  'focus:bg-gray-300 dark:focus:bg-gray-600',
+].join(' ')
+
+const buttonActiveClass = 'bg-gray-300 dark:bg-gray-600 cursor-default'
+const buttonInactiveClass = 'bg-gray-200 dark:bg-gray-800 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700'
+const buttonClass = `${buttonBaseClass} ${props.active ? buttonActiveClass : buttonInactiveClass}`
 
 const emit = defineEmits<{
   click: []
@@ -17,10 +34,7 @@ const handleClick = () => {
 
 <template>
   <button
-    class="px-3 py-1.5 text-gray-600 dark:text-gray-300 rounded-md text-sm transition-colors relative"
-    :class="active
-      ? 'bg-gray-300 dark:bg-gray-600 cursor-default'
-      : 'bg-gray-200 dark:bg-gray-800 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700'"
+    :class="buttonClass"
     :aria-label="`Search for ${term}`"
     @click="handleClick"
   >
