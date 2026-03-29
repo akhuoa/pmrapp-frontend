390ff0ec64de0458cb52240dc33e31d1590891c4
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-03-30 12:06:45 +1300

    Add a wrap icon in wrap button

--- src/components/atoms/WrapButton.vue
+++ src/components/atoms/WrapButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import CopyIcon from '@/components/icons/CopyIcon.vue'
+import WrapIcon from '@/components/icons/WrapIcon.vue'
 
 const props = defineProps<{
   text: string
@@ -20,6 +20,6 @@ const handleClick = () => {
     class="relative p-2 text-gray-500 cursor-pointer hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 rounded transition-colors"
     title="Wrap"
   >
-    <CopyIcon class="w-4 h-4" />
+    <WrapIcon class="w-4 h-4" />
   </button>
 </template>
