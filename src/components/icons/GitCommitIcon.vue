4e1c057fb8c257ad8fbc4ec26927d19d0c4e0fe9
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-05-04 18:30:21 +1200

    Add git icons

---
+++ src/components/icons/GitCommitIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ class?: string }>(), {
+  class: 'w-6 h-6',
+})
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    :class="$props.class"
+    aria-hidden="true"
+  >
+    <circle cx="12" cy="12" r="3" />
+    <path d="M3 12h6" />
+    <path d="M15 12h6" />
+  </svg>
+</template>
