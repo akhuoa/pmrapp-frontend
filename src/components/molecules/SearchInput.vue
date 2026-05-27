4b7ee81d1092a1e8d5bd298e473560564f681891
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-05-27 15:33:44 +1200

    Refactor code for repeated templates

--- src/components/molecules/SearchInput.vue
+++ src/components/molecules/SearchInput.vue
@@ -9,7 +9,7 @@ import {
   watch,
 } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import Keycap from '@/components/atoms/Keycap.vue'
+import SearchEnterHint from '@/components/atoms/SearchEnterHint.vue'
 import SearchField from '@/components/atoms/SearchField.vue'
 import TermButton from '@/components/atoms/TermButton.vue'
 import { SEARCH_CATEGORIES } from '@/constants/search'
@@ -333,17 +333,13 @@ defineExpose({
           <p class="text-gray-500 dark:text-gray-400 text-sm">
             No keywords or authors found for
             <span class="text-gray-700 dark:text-gray-200 font-semibold">"{{ searchInput }}"</span>.
-            Press <Keycap>Enter</Keycap> to search for
-            <span class="text-gray-700 dark:text-gray-200 font-semibold">"{{ searchInput }}"</span>
-            anywhere in the repository.
+            <SearchEnterHint :query="searchInput" />
           </p>
         </div>
         <div v-else class="max-h-96 overflow-y-auto scrollbar-thin group/results">
           <div class="p-4 border-b border-gray-200 dark:border-gray-700" v-if="!props.inOverlay">
             <p class="text-gray-500 dark:text-gray-400 text-sm">
-              Press <Keycap>Enter</Keycap> to search for
-              <span class="text-gray-700 dark:text-gray-200 font-semibold">"{{ searchInput }}"</span>
-              anywhere in the repository{{ termTypeSuffix }}.
+              <SearchEnterHint :query="searchInput" :suffix="termTypeSuffix" />
             </p>
           </div>
           <div
