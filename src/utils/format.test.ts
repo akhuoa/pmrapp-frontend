58bf4e8ea3c5d55f3327c3ef2a1d730dc324f3b5
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-06-03 16:02:32 +1200

    Add tests for format util

---
+++ src/utils/format.test.ts
@@ -0,0 +1,93 @@
+import { describe, expect, it } from 'vitest'
+import {
+  formatNumber,
+  formatDate,
+  formatFileCount,
+  formatSearchKey,
+  formatTermKey,
+  formatKindLabel,
+} from './format'
+
+describe('formatNumber', () => {
+  it('returns formatted number', () => {
+    const results = formatNumber(1234567)
+
+    expect(results).toBe('1,234,567')
+  })
+})
+
+describe('formatDate', () => {
+  it('returns formatted date', () => {
+    const results = formatDate(1704499200)
+
+    expect(results).toBe('6 January 2024')
+  })
+})
+
+describe('formatFileCount', () => {
+  it('returns formatted file count for 1 item', () => {
+    const results = formatFileCount(1)
+
+    expect(results).toBe('1 item')
+  })
+
+  it('returns formatted file count for more than 1', () => {
+    const results = formatFileCount(1234)
+
+    expect(results).toBe('1,234 items')
+  })
+
+  it('returns formatted file count for zero', () => {
+    const results = formatFileCount(0)
+
+    expect(results).toBe('')
+  })
+
+  it('returns formatted file count for undefined', () => {
+    const results = formatFileCount(undefined)
+
+    expect(results).toBe('')
+  })
+
+  it('returns formatted file count for null', () => {
+    const results = formatFileCount(null)
+
+    expect(results).toBe('')
+  })
+})
+
+describe('formatSearchKey', () => {
+  it('returns formatted search key for normal string', () => {
+    const results = formatSearchKey('Cells')
+
+    expect(results).toBe('<span class="text-gray-700 dark:text-gray-200 font-semibold"><em>Cells</em></span>')
+  })
+
+  it('returns formatted and escaped HTML for HTML string', () => {
+    const results = formatSearchKey('<h1>Cells</h1>')
+
+    expect(results).toBe('<span class="text-gray-700 dark:text-gray-200 font-semibold"><em>&lt;h1&gt;Cells&lt;/h1&gt;</em></span>')
+  })
+
+  it('returns formatted and escaped HTML for HTML string', () => {
+    const results = formatSearchKey('<script>alert("hello")</script>')
+
+    expect(results).toBe('<span class="text-gray-700 dark:text-gray-200 font-semibold"><em>&lt;script&gt;alert(&quot;hello&quot;)&lt;/script&gt;</em></span>')
+  })
+})
+
+describe('formatTermKey', () => {
+  it('returns formatted term key for normal string', () => {
+    const results = formatTermKey('Cells')
+
+    expect(results).toBe('<span class="text-gray-700 dark:text-gray-200 font-semibold">Cells</span>')
+  })
+})
+
+describe('formatKindLabel', () => {
+  it('returns formatted kind label for normal string', () => {
+    const results = formatKindLabel('Cells')
+
+    expect(results).toBe('<em class="text-gray-600 dark:text-gray-300">Cells</em>')
+  })
+})
