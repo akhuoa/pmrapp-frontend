2d7c3fce515abcb6c865e1c854da5d379efe3376
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-06-19 12:12:07 +1200

    Add a comment for citation URL generator

--- src/components/organisms/ExposureDetail.vue
+++ src/components/organisms/ExposureDetail.vue
@@ -137,6 +137,9 @@ const fileBrowserPath = computed(() => {
   return typeof p === 'string' ? p : undefined
 })
 
+// Generates a citation URL from the current route, excluding query parameters and the hash.
+// It resolves the application's base path from vite.config to construct the complete URL.
+// It also maintains reactivity for navigations.
 const citationUrl = computed(() => {
   const resolved = router.resolve({ name: route.name, params: route.params })
   const resolvedURL = new URL(resolved.href, window.location.origin)
